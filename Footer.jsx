e3799cde7bed4845a82c130cb9eb37c033eeaecd
khushalee1996 <86585841+khushalee1996@users.noreply.github.com>
2021-06-28 15:07:25 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function footer() {
+  return (
+    <footer>
+      <p> CopyrightByShapeAI @ {new Date().getFullYear()} </p>
+    </footer>
+  );
+}
+export default footer;
